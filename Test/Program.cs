d415d35c708cb3f5af6cefa4599185c1e7d92a63
Ruchkin Aleksandr <sasharuchkin2006@vk.com>
2026-04-23 14:16:26 +0300

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var os = Environment.OSVersion;
+            Console.WriteLine($"Платформа: {os.Platform}");
+            Console.WriteLine($"Версия: {os.Version}");
+            Console.WriteLine($"Полная строка: {os.VersionString}");
+        }
+    }
+}
